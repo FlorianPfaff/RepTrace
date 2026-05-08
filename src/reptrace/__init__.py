9c9b279136676195abbbf339695657d25eda4eba
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-08 03:53:47 +0200

    Install event detection extensions

--- src/reptrace/__init__.py
+++ src/reptrace/__init__.py
@@ -2,3 +2,8 @@
 
 __all__ = ["__version__"]
 __version__ = "0.1.0"
+
+from reptrace._event_detection_extensions import install as _install_event_detection_extensions
+
+_install_event_detection_extensions()
+del _install_event_detection_extensions
